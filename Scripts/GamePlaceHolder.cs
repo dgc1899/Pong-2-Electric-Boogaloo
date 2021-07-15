7f9ecd707089c51a74d9bd19265a45e4caa19d55
dgc1899 <daniel.garcia18@tectijuana.edu.mx>
2021-07-14 18:28:52 -0700

    First push Menu_Scene

---
+++ Scripts/GamePlaceHolder.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public class GamePlaceHolder : Node2D
+{
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    public void _on_Button_pressed()
+    {
+        GetTree().ChangeScene("res://Scenes/MainMenu.tscn");
+    }
+}
---
+++ Scripts/MenuButton.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public class MenuButton : Button
+{
+    //Export string variable to capture the node it'll change to
+    [Export]
+    public PackedScene SceneToLoad { get; set; }
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+
+}
